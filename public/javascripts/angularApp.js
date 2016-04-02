661a29c04963725540f353768981b0da9e150772
Emalsha <remalsha@gmail.com>
2016-04-02 17:59:34 +0530

    error changed

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -9,69 +9,38 @@ function appController($scope,$http){
     $scope.isTableShow = false;
 
     $scope.searchResult=function(){
-        var index ={ index:$scope.index_number };
-        $http.post('/',index).then(
-            function successCallback(response) {
-                $scope.result_1 = response.data[0];
-                $scope.result_2 = response.data[1];
-                //$scope.res = response.data[1];
+        $scope.isTableShow = false;
+        var index_num = $scope.index_number;
+        var index ={ index:index_num };
 
-                $scope.isTableShow = true;
-                if($scope.result_1.enhancement === 1){
-                    $scope.enhan_1 = "glyphicon glyphicon-ok";
-                }else if($scope.result_1.enhancement === 0){
-                    $scope.enhan_1 ="glyphicon glyphicon-remove";
-                }else{
-                    $scope.enhan_1 = "glyphicon glyphicon-question-sign";
-                }
-            },
-            function errorCallback(response) {
-                alert("error occurred during getting data.");
-            }
-        );
+        if (index_num < 0 || index_num.toString().length != 8) {
+            alert("Please enter valid index number.");
+        }else if(index_num.toString()[3] != 2) {
+            alert("Sorry , This service valid for only IS students.");
+        }else{
+            $http.post('/',index).then(
+                function successCallback(response) {
+                    if (response.data != "") {
+                        $scope.result_1 = response.data[0];
+                        $scope.result_2 = response.data[1];
+                        /*$scope.res = response.data;*/
 
-    };
-}
-
-/*var app = angular.module('app', []);
-
-app.controller('appController', ['$scope', '$http', appController]);
-	
-function appController($scope, $http) {
-	
-    //--- Essential variables
-	$scope.index_number = "";
-    $scope.isTableShow = false;
-	
-	$scope.searchResult = function(index_number,semester_1,semester_2){
-            if(index_number !==""){
-                angular.forEach($scope.semester_1, function(value, key) {
-                    if (value.Index_num === index_number) {
-                        $scope.result_1 = value;
                         $scope.isTableShow = true;
-                        if(value.enhancement === 1){
+                        if($scope.result_1.enhancement === 1){
                             $scope.enhan_1 = "glyphicon glyphicon-ok";
-                        }else if(value.enhancement === 0){
+                        }else if($scope.result_1.enhancement === 0){
                             $scope.enhan_1 ="glyphicon glyphicon-remove";
                         }else{
                             $scope.enhan_1 = "glyphicon glyphicon-question-sign";
                         }
+                    }else{
+                        alert("Please check your index number.");
                     }
-                });
-                
-                angular.forEach($scope.semester_2, function(value,key){
-                    if (value.Index_num === index_number) {
-                        $scope.result_2 = value;
-                        $scope.isTableShow = true;
-                        if(value.enhancement === 1){
-                            $scope.enhan_2 = "glyphicon glyphicon-ok";
-                        }else if(value.enhancement === 0){
-                            $scope.enhan_2 ="glyphicon glyphicon-remove";
-                        }else{
-                            $scope.enhan_2 ="glyphicon glyphicon-question-sign";
-                        }
-                    }
-                });
-            }
+                },
+                function errorCallback(response) {
+                    alert("error occurred during getting data.");
+                }
+            );
+        }
     };
-}*/
+}
